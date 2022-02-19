3abe573e6592dea8dfb17b9c6961bfa5983003b8
ting <thetingzhang@gmail.com>
2022-02-19 15:36:10 -0500

    Implement (#1)
    * [update] docs
    
    * [add] scipy
    
    * [add] func signature
    
    * [add] psuedocode
    
    * [add] param checks
    
    * [add] simple test
    
    * [add] implement logic with stubbed helpers
    
    * [add] implement stubbed funcs
    
    * [update] use list of points indexed by centroid
    
    * [add] running script
    
    * [remove] unused import

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # kmeans
+
+## usage
+1. install python dependencies
+```
+install python dependencies
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+2. run kmeans script
+```
+python kmeans.py
+```
---
+++ kmeans.py
@@ -0,0 +1,84 @@
+from random import sample
+from typing import List
+
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+Point = List[int]
+
+
+def assign_cluster(point: Point, centroids: List[Point]) -> int:
+    """
+    return index of closest centroid for a given point
+    """
+    min_distance = float("inf")
+    closest_centroid_idx = None
+    for idx, centroid in enumerate(centroids):
+        distance = euclidean(point, centroid)
+        if distance < min_distance:
+            min_distance = distance
+            closest_centroid_idx = idx
+    return closest_centroid_idx
+
+
+def calculate_variation(points: List[Point]) -> float:
+    """
+    return the maximum distance between any two points in a list
+    """
+    max_distance = 0
+    for i in range(len(points)):
+        for j in range(len(points)):
+            distance = euclidean(points[i], points[j])
+            max_distance = distance if distance > max_distance else max_distance
+
+    return max_distance
+
+
+def kmeans(
+    points: List[Point],
+    k: int = 3,
+    runs: int = 3,
+    max_iter: int = 100,
+) -> List[Point]:
+    """
+    return a list of cluster centroids, given a list of points and the
+    number of clusters to identify
+    """
+    if k > len(points):
+        raise ValueError("number of clusters (k) must be less than number of points")
+
+    dimensions = {len(p) for p in points}
+    if len(dimensions) > 1:
+        raise ValueError("points must all have the same dimensions")
+
+    min_variation: float = float("inf")
+    best_cluster: List[Point] = None
+
+    for i in range(runs):
+        centroids = sample(points, k)
+        for i in range(max_iter):
+            clusters = [[] for c in centroids]
+            for p in points:
+                closest_centroid_idx = assign_cluster(p, centroids)
+                clusters[closest_centroid_idx].append(p)
+
+            means = [np.mean(c, axis=0).tolist() for c in clusters]
+
+            centroids = means
+
+        variation = sum([calculate_variation(c) for c in clusters])
+        if variation < min_variation:
+            min_variation = variation
+            best_cluster = centroids
+
+    return best_cluster
+
+
+if __name__ == "__main__":
+    # test data are one-dimensional points with three clear centroids at 2, 12, and 22
+    points = [[1], [2], [3], [11], [12], [13], [21], [22], [23]]
+    centroids = kmeans(points, k=3)
+    assert len(centroids) == 3
+    assert [2] in centroids
+    assert [12] in centroids
+    assert [22] in centroids
