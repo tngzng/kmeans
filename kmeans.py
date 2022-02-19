869d676e27a95ffad154026e568707dbfb1c441e
ting zhang <thetingzhang@gmail.com>
2022-02-19 13:48:58 -0500

    [add] func signature

---
+++ kmeans.py
@@ -0,0 +1,11 @@
+from typing import List
+
+Point = List[int]
+
+
+def kmeans(k: int, points: List[Point]) -> List[Point]:
+    """
+    return a list of cluster centroids, given a list of points and the
+    number of clusters to identify
+    """
+    pass
