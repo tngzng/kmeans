3a5f1ffbbeb33fa814fb154b478f2d5be87be4b9
ting zhang <thetingzhang@gmail.com>
2022-02-19 14:01:50 -0500

    [add] psuedocode

--- kmeans.py
+++ kmeans.py
@@ -3,9 +3,25 @@ from typing import List
 Point = List[int]
 
 
-def kmeans(k: int, points: List[Point]) -> List[Point]:
+def kmeans(points: List[Point], k: int = 3, max_iter: int = 100) -> List[Point]:
     """
     return a list of cluster centroids, given a list of points and the
     number of clusters to identify
     """
-    pass
+    # check that all points in List[Point] are of the same dimensions (same length)
+
+    # min_variation: float = infinity
+    # best_cluster: List[Point] = None
+
+    # for i in max_iter:
+    #   randomly initialize k centroids of the same dimension as the points in List[Point]
+    #   may want to add random_seed param for testing
+    #   while true:
+    #       iterate through the points and assign each to the nearest centroid
+    #       determine the mean of each cluster and reassign the centroid value to the mean
+    #       break when centroids converge (previous centroid values equal the new centroid values)
+
+    #   calculate the sum of variation of the points in the current centroids' clusters
+    #   if the sum of variation is less than min_variation, reassign min_variation and best_cluster
+
+    # return best_cluster
